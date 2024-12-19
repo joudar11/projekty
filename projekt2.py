4673fb2c8c345d08b07ba9ae3501e57c3b300a14
joudar11 <krystofklika@pm.me>
2024-12-19 17:57:19 +0100

    optimalizace

--- projekt2.py
+++ projekt2.py
@@ -11,7 +11,7 @@ correct = False
 
 #str - vraci nahodne ctyrmistne cislo k hadani hracem, ktere je unikatni a nezacina nulou
 def to_guess():
-    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    numbers = list(range(0, 10))
     increments = [1000, 100, 10, 1]
     number = 0
 
